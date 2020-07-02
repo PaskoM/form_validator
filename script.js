e0a39bd273fad75e61b10b52486906752e60564c
Mariola Pasko <paskomariola@gmail.com>
2020-07-02 23:28:34 +0200

    validations

---
+++ script.js
@@ -0,0 +1,26 @@
+cont form = document.getElementById('form');
+cont username = document.getElementById('username');
+cont email = document.getElementById('email');
+cont password = document.getElementById('password');
+cont password = document.getElementById('password2');
+
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
+}
